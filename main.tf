f7f365fb87ffa2bfbf953efa49fd632fb37e400f
seshimo8 <seshimo3594@gmail.com>
2025-03-08 21:31:41 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  cloud {
+    organization = "terraform-seshimo"
+
+    workspaces {
+      name = "test"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
