0e6435b9729736622b3e634f08b3087f6d2914c4
seshimo8 <seshimo3594@gmail.com>
2025-03-15 11:59:48 +0900

    delete: vpc

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "example"
-  }
-}
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "example"
+#   }
+# }
